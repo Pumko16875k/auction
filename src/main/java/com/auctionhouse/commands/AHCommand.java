7ed65901554ba9a3042bd9180e0abec941bfeeca
Pumko16875k <marinsousmarin@gmail.com>
2026-08-12 04:57:28 +0200

    Create AHCommand.java

---
+++ src/main/java/com/auctionhouse/commands/AHCommand.java
@@ -0,0 +1,51 @@
+package com.auctionhouse.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.StringTextComponent;
+
+public class AHCommand {
+
+    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+        dispatcher.register(Commands.literal("ah")
+            .executes(context -> {
+                ServerPlayerEntity player = context.getSource().asPlayer();
+                player.sendMessage(new StringTextComponent("§aOuverture de l'Hôtel de Ventes..."), player.getUUID());
+                return 1;
+            })
+            .then(Commands.literal("sell")
+                .then(Commands.argument("price", DoubleArgumentType.doubleArg(0.01))
+                    .executes(context -> {
+                        double price = DoubleArgumentType.getDouble(context, "price");
+                        return processSell(context.getSource().asPlayer(), price, 7);
+                    })
+                    .then(Commands.argument("days", IntegerArgumentType.integer(1, 100))
+                        .executes(context -> {
+                            double price = DoubleArgumentType.getDouble(context, "price");
+                            int days = IntegerArgumentType.getInteger(context, "days");
+                            return processSell(context.getSource().asPlayer(), price, days);
+                        })
+                    )
+                )
+            )
+        );
+    }
+
+    private static int processSell(ServerPlayerEntity player, double price, int days) {
+        ItemStack heldItem = player.getMainHandItem();
+        
+        if (heldItem.isEmpty()) {
+            player.sendMessage(new StringTextComponent("§cVous devez tenir un item en main !"), player.getUUID());
+            return 0;
+        }
+
+        heldItem.shrink(heldItem.getCount()); 
+        player.sendMessage(new StringTextComponent("§aItem mis en vente pour " + price + "$ pendant " + days + " jours !"), player.getUUID());
+        return 1;
+    }
+}
